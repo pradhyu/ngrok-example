474c9326cb07005d4670a494689f2ffd9cf340d0
PK <pradhyu.shrestha@gmail.com>
2019-05-09 01:07:48 -0400

    product.jston

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.debug = True
 @app.route('/')
 def main():
-    with open('compute-api.json', 'r') as myfile:
+    with open('product.json', 'r') as myfile:
         data = myfile.read()
     return data
 if __name__ == '__main__':
- app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
